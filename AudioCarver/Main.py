987532c2ed6c7cdbfe3ae56fea96a58447f83bbc
Aloosha2 <alicjam@uw.edu>
2024-07-08 13:50:18 +0200

    created library

---
+++ AudioCarver/Main.py
@@ -0,0 +1,30 @@
+import sys
+from Audio_Conversion import *
+from Seam_Carving import carve_audio
+
+def main(input_wavfile, n_of_seams):
+    # Load the audio file
+    audio, sampling_rate = load_wavfile(input_wavfile)
+    matrix = get_stft(audio)
+    magnitude, phase = extract_mag_pha(matrix)
+    
+    # Carve the audio
+    magnitude, phase = carve_audio(n_of_seams, magnitude, phase)
+    
+    # Create the output filename
+    output_filename = f'{input_wavfile.split(".")[0]}_carved{n_of_seams}.wav'
+    
+    # Save the carved audio to a new wav file
+    sig_to_wav(output_filename, magnitude, phase)
+    
+    print(f"Processed {input_wavfile} and saved as {output_filename} with {n_of_seams} seams carved")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <input_wavfile> <n_of_seams>")
+        sys.exit(1)
+    
+    input_wavfile = sys.argv[1]
+    n_of_seams = int(sys.argv[2])
+    
+    main(input_wavfile, n_of_seams)
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # AudioCarver
 Seam Carving as a Data Augmentation Technique 
+
+# Audio Processing Library
+
+A Python library for performing seam carving on audio files, converting audio to matrix and back, and generating spectrograms.
+
+![Build Status](https://img.shields.io/github/workflow/status/yourusername/audio_processing/CI)
+![License](https://img.shields.io/github/license/yourusername/audio_processing)
+![PyPI version](https://img.shields.io/pypi/v/audio_processing)
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [Examples](#examples)
+- [Documentation](#documentation)
+- [Contributing](#contributing)
+- [License](#license)
+- [Contact](#contact)
+
+## Installation
+
+You can install the library using pip:
+
+```sh
+pip install audio_processing
