9054564bd45f096e6082cbe5eda5dbebc078dcbd
Aloosha2 <alicjam@uw.edu>
2024-09-06 11:33:00 +0200

    updated seam_carving.py

--- audio_carver/seam_carving.py
+++ audio_carver/seam_carving.py
@@ -3,49 +3,19 @@ from numba import njit
 
 @njit
 def min_vertical_seam_energy(energies):
-    height, width = energies.shape
-    seam_energies = np.zeros((height, width))
-    back_pointers = np.zeros((height, width), dtype=np.int32)
-
-    seam_energies[0] = energies[0]
-
-    for y in range(1, height):
-        for x in range(width):
-            x_left = max(x - 1, 0)
-            x_right = min(x + 1, width - 1)
-            min_parent_x = x_left + np.argmin(seam_energies[y - 1, x_left:x_right + 1])
-
-            seam_energies[y, x] = energies[y, x] + seam_energies[y - 1, min_parent_x]
-            back_pointers[y, x] = min_parent_x
-
+    height, seam_energies, back_pointers = dp_seam_find(energies)
     min_energy = np.min(seam_energies[-1])
     return min_energy
 
 @njit
 def find_vertical_seam(energies):
-    height, width = energies.shape
-    seam_energies = np.zeros((height, width))
-    back_pointers = np.zeros((height, width), dtype=np.int32)
-
-    seam_energies[0] = energies[0]
-
-    for y in range(1, height):
-        for x in range(width):
-            x_left = max(x - 1, 0)
-            x_right = min(x + 1, width - 1)
-            min_parent_x = x_left + np.argmin(seam_energies[y - 1, x_left:x_right + 1])
-
-            seam_energies[y, x] = energies[y, x] + seam_energies[y - 1, min_parent_x]
-            back_pointers[y, x] = min_parent_x
-
+    height, seam_energies, back_pointers = dp_seam_find(energies)
     min_seam_end_x = np.argmin(seam_energies[-1])
     seam = []
-
     seam_point_x = min_seam_end_x
     for y in range(height - 1, -1, -1):
         seam.append((seam_point_x, y))
         seam_point_x = back_pointers[y, seam_point_x]
-
     seam.reverse()
     return seam
 
@@ -80,3 +50,21 @@ def carve_audio(n_of_seams, magnitude, phase, is_vertical=True):
             phase = np.pad(phase, ((0, pad_height), (0, 0)), mode='constant')
 
     return magnitude, phase
+
+@njit
+def dp_seam_find(energies):
+    height, width = energies.shape
+    seam_energies = np.zeros((height, width))
+    back_pointers = np.zeros((height, width), dtype=np.int32)
+
+    seam_energies[0] = energies[0]
+
+    for y in range(1, height):
+        for x in range(width):
+            x_left = max(x - 1, 0)
+            x_right = min(x + 1, width - 1)
+            min_parent_x = x_left + np.argmin(seam_energies[y - 1, x_left:x_right + 1])
+
+            seam_energies[y, x] = energies[y, x] + seam_energies[y - 1, min_parent_x]
+            back_pointers[y, x] = min_parent_x
+    return height, seam_energies, back_pointers
